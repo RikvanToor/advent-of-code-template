7e440926072d6c5fee5c8997f056eaa262e74741
Rik van Toor <rik.van.toor@gmail.com>
2020-12-07 12:52:19 +0100

    Day 7

--- src/Days/Day07.hs
+++ src/Days/Day07.hs
@@ -1,39 +1,59 @@
 module Days.Day07 (runDay) where
 
-{- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
+import           Control.Applicative  ( (<|>) )
+import           Data.Tree
+import           Data.List
+import           Data.Map.Strict      ( Map )
 import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
-import qualified Program.RunDay as R (runDay)
-import Data.Attoparsec.Text
-import Data.Void
-{- ORMOLU_ENABLE -}
+import qualified Program.RunDay as R ( runDay )
+import           Data.Attoparsec.Text
 
 runDay :: Bool -> String -> IO ()
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = many' l
+  where l = Rule <$> manyTill anyChar (string " bags contain ") <*> (e <|> manyTill r (endOfLine <|> endOfInput))
+        r = (,) <$ many' space <*> decimal <* many' space <*> manyTill anyChar ((string " bags" <|> string " bag") <* (string ", " <|> string "."))
+        e = [] <$ string "no other bags." <* (endOfLine <|> endOfInput)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Rule]
 
-type OutputA = Void
+data Rule = Rule
+  { ruleColour   :: String
+  , ruleContains :: [(Int, String)]
+  } deriving Show
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
+getColours :: Input -> [String] -> [String]
+getColours is possibs =
+  let newPossibs = map ruleColour $ filter (any ((`elem` possibs) . snd) . ruleContains) is
+      total = nub $ possibs ++ newPossibs
+  in if possibs == total then possibs else getColours is total
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA rules = length (getColours rules ["shiny gold"]) - 1
 
 ------------ PART B ------------
+mapToTree :: Map String [(Int, String)] -> String -> Maybe (Tree String)
+mapToTree m rt = do
+  rs <- Map.lookup rt m
+  ns <- sequenceA $ concatMap (\(i, s) -> replicate i (mapToTree m s)) rs
+  return $ Node rt ns
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB is =
+  let m  = Map.fromList (map (\r -> (ruleColour r, ruleContains r)) is)
+      m' = Map.update (const (Just [])) "faded blue"
+         $ Map.update (const (Just [])) "dotted black"
+         $ Map.update (const (Just [(5, "faded blue"), (6, "dotted black")])) "vibrant plum"
+         $ Map.update (const (Just [(3, "faded blue"), (4, "dotted black")])) "dark olive"
+         $ m
+  in maybe 0 (\x -> length x - 1) $ mapToTree m' "shiny gold"
