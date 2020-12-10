de8ab24b89459f19c6c53be63c81274797bb32ef
Rik van Toor <rik.van.toor@gmail.com>
2020-12-10 10:34:00 +0100

    Day 10

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -1,39 +1,54 @@
-module Days.Day10 (runDay) where
+module Days.Day10 where
 
-{- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Data.List  ( sortOn, sort )
+import Data.Maybe ( fromMaybe, listToMaybe )
+import Data.Ord   ( Down(..) )
 
 import qualified Program.RunDay as R (runDay)
-import Data.Attoparsec.Text
-import Data.Void
-{- ORMOLU_ENABLE -}
+import Data.Attoparsec.Text hiding (takeWhile, take)
 
 runDay :: Bool -> String -> IO ()
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = decimal `sepBy` space
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Int]
 
-type OutputA = Void
+data Accum = Accum
+  { ones   :: Int
+  , twos   :: Int
+  , threes :: Int
+  }
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
+bumpAccum :: Int -> Int -> Accum -> Accum
+bumpAccum x y a@Accum{..} = case abs (x - y) of
+  1 -> a{ones = ones + 1}
+  2 -> a{twos = twos + 1}
+  3 -> a{threes = threes + 1}
+  _ -> a
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA as =
+  let builtin = maximum as + 3
+      as'     = builtin : sortOn Down as
+      res     = snd $ foldr (\x (y, accum) -> (x, bumpAccum x y accum)) (0, Accum 0 0 0) as'
+  in ones res * threes res
+
 
 ------------ PART B ------------
+countPaths :: [Int] -> Int
+countPaths = fromMaybe 0 . listToMaybe . snd . foldr go ([], [])
+  where go x (xs, res) =
+          let options = length $ takeWhile (<= x+3) xs
+          in (x:xs, max 1 (sum (take options res)) : res)
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = countPaths . (0:) . sort
