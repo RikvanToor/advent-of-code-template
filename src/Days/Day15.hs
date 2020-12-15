81586e83317edd6990575b496bc8cceae74f668a
Rik van Toor <rik.van.toor@gmail.com>
2020-12-15 23:54:48 +0100

    Copied a VERY fast solution from /u/segft on Reddit

--- src/Days/Day15.hs
+++ src/Days/Day15.hs
@@ -1,8 +1,9 @@
 module Days.Day15 (runDay) where
 
-import           Data.Map.Strict     ( Map )
-import qualified Data.Map.Strict as Map
-import qualified Util.Util as U
+import           Control.Monad       ( forM_ )
+import           Control.Monad.ST    ( ST, runST )
+import           Data.Foldable       ( foldlM )
+import qualified Data.Vector.Unboxed.Mutable as VM
 
 import qualified Program.RunDay as R ( runDay )
 import           Data.Attoparsec.Text hiding ( take )
@@ -22,29 +23,22 @@ type OutputA = Int
 type OutputB = Int
 
 ------------ PART A ------------
-step :: Int -> Int -> Map Int [Int] -> (Int, Int, Map Int [Int])
-step turn i m = 
-  let newNum = case take 2 <$> Map.lookup i m of
-        (Just [x,y]) -> x-y
-        _            -> 0
-  in (turn+1, newNum, Map.insertWith (\new old -> new ++ take 1 old) newNum [turn] m)
-
-stepUntilTurn :: Int -> Int -> Int -> Map Int [Int] -> Int
-stepUntilTurn maxTurn turn i m
-  | turn == maxTurn + 1 = i
-  | otherwise = U.uncurry3 (stepUntilTurn maxTurn) (step turn i m)
-
-initAndRunUntil :: Int -> Input -> Int
-initAndRunUntil maxTurn is =
-  let m     = Map.fromList $ zipWith (flip (,)) (map pure [1..] :: [[Int]]) is
-      lastN = last is
-      turn  = length is + 1
-  in stepUntilTurn maxTurn turn lastN m
-
 partA :: Input -> OutputA
-partA = initAndRunUntil 2020
-
+partA = run 2020
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = initAndRunUntil 30000000
+partB = run 30000000
+
+run :: Int -> [Int] -> Int
+run target input = runST $ do
+  v <- VM.replicate target 0
+  forM_ (zip (init input) [1..]) $ uncurry (VM.write v)
+  let len = length input
+  foldlM (speakNum v) (last input) [len..target-1]
+
+speakNum :: VM.MVector s Int -> Int -> Int -> ST s Int
+speakNum v prev i = do
+  prevPos <- VM.read v prev
+  VM.write v prev i
+  return $ if prevPos == 0 then 0 else i - prevPos
