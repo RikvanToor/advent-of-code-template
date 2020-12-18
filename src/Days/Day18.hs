ee0ce75a77d93f89cc90c2efea5c1d5a4e9c089c
Rik van Toor <rik.van.toor@gmail.com>
2020-12-18 09:22:59 +0100

    Day 18. Ugly.

--- src/Days/Day18.hs
+++ src/Days/Day18.hs
@@ -1,39 +1,92 @@
 module Days.Day18 (runDay) where
 
-{- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import           Control.Applicative  ( (<|>) )
+import           Data.List            ( elemIndices, findIndices )
+import           Data.Maybe           ( listToMaybe )
 
-import qualified Program.RunDay as R (runDay)
-import Data.Attoparsec.Text
-import Data.Void
-{- ORMOLU_ENABLE -}
+import qualified Program.RunDay as R  (runDay)
+import Data.Attoparsec.Text hiding ( take )
 
 runDay :: Bool -> String -> IO ()
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = symbsParser `sepBy` endOfLine
+
+symbsParser :: Parser [Symb]
+symbsParser = symb
+  where symb = (num <|> plus <|> times <|> parens) `sepBy` char ' '
+        num = Num' <$> decimal
+        plus = Plus' <$ char '+'
+        times = Times' <$ char '*'
+        parens = SubExp <$ char '(' <*> symb <* char ')'
+
+processSymbs :: [Symb] -> Either String Expr
+processSymbs ss =
+  case listToMaybe $ reverse $ findIndices (\s -> s == Plus' || s == Times') ss of
+    Just x -> do
+      a <- processSymbs (take x ss)
+      b <- processSymbs (drop (x+1) ss)
+      case ss !! x of
+        Plus'  -> return $ Plus a b
+        Times' -> return $ Times a b
+        _      -> Left "Not an operator"
+    Nothing -> case ss of
+      [Num' i]     -> return $ Num i
+      [SubExp ss'] -> processSymbs ss'
+      _ -> Left "Multiple symbs left, but no operator in between"
+
+processSymbs2 :: [Symb] -> Either String Expr
+processSymbs2 ss =
+  case listToMaybe $ reverse $ elemIndices Times' ss of
+    Just x -> do
+      a <- processSymbs2 (take x ss)
+      b <- processSymbs2 (drop (x+1) ss)
+      case ss !! x of
+        Times'  -> return $ Times a b
+        _      -> Left "Not an operator"
+    Nothing -> 
+      case listToMaybe $ reverse $ elemIndices Plus' ss of
+        Just x -> do
+          a <- processSymbs2 (take x ss)
+          b <- processSymbs2 (drop (x+1) ss)
+          case ss !! x of
+            Plus'  -> return $ Plus a b
+            _      -> Left "Not an operator"
+        Nothing ->
+          case ss of
+            [Num' i]     -> return $ Num i
+            [SubExp ss'] -> processSymbs2 ss'
+            _ -> Left "Multiple symbs left, but no operator in between"
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [[Symb]]
 
-type OutputA = Void
+data Symb = Num' Int
+          | Plus'
+          | Times'
+          | SubExp [Symb]
+  deriving (Show, Eq)
 
-type OutputB = Void
+data Expr = Num Int
+          | Plus Expr Expr
+          | Times Expr Expr
+  deriving Show
+
+type OutputA = Either String Int
+
+type OutputB = Either String Int
 
 ------------ PART A ------------
+eval :: Expr -> Int
+eval (Num i)     = i
+eval (Plus a b)  = eval a + eval b
+eval (Times a b) = eval a * eval b
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = fmap (sum . map eval) . traverse processSymbs . filter (not . null)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = fmap (sum . map eval) . traverse processSymbs2 . filter (not . null)
