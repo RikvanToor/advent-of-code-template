6b1119fd1f64e3b02a501786bcfbdfa4736b5da5
Rik van Toor <rik.van.toor@gmail.com>
2020-12-26 01:33:49 +0100

    Day 11 stylistic improvements

--- src/Days/Day11.hs
+++ src/Days/Day11.hs
@@ -1,20 +1,13 @@
 module Days.Day11 (runDay) where
 
-{- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
+import Data.List                        ( find )
+import Data.Map.Strict                  ( Map )
 import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
+import Data.Maybe                       ( mapMaybe )
 import qualified Util.Util as U
 
-import qualified Program.RunDay as R (runDay)
+import qualified Program.RunDay as R    ( runDay )
 import Data.Attoparsec.Text
-import Data.Void
-{- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
 runDay = R.runDay inputParser partA partB
@@ -89,16 +82,6 @@ stepB i = Map.mapWithKey go i
             Floor -> Floor
             Empty -> if nrOfTakens == 0 then Taken else Empty
             Taken -> if nrOfTakens >= 5 then Empty else Taken
-
-printInput :: Input -> String
-printInput i = unlines $ map line [0..maxY]
-  where maxX = maximum (map fst $ Map.keys i)
-        maxY = maximum (map snd $ Map.keys i)
-        line y = map (printTile . (`Map.lookup` i) . (,y)) [0..maxX]
-        printTile (Just Floor) = '.'
-        printTile (Just Empty) = 'L'
-        printTile (Just Taken) = '#'
-        printTile Nothing      = '☐'
 
 partB :: Input -> OutputB
 partB = length . Map.filter (== Taken) . U.fix stepB
