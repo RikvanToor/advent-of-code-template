218e95d270ea44a3aff203cbade5d627a12957cc
Rik van Toor <rik.van.toor@gmail.com>
2020-12-26 01:31:37 +0100

    Day 6

--- src/Days/Day06.hs
+++ src/Days/Day06.hs
@@ -1,39 +1,31 @@
 module Days.Day06 (runDay) where
 
-{- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
+import Data.Set                      ( Set )
 import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
-import qualified Program.RunDay as R (runDay)
+import qualified Program.RunDay as R ( runDay )
 import Data.Attoparsec.Text
-import Data.Void
-{- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = many' g
+  where l = Set.fromList <$> manyTill letter endOfLine
+        g = manyTill l endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [[Set Char]]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = sum . map (length . Set.unions)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = sum . map (length . foldr1 Set.intersection)
